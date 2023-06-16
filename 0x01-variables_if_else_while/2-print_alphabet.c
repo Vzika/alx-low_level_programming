89e7a445287312b87d95a1e11136bec91e91be1e
Vzika <ngozi@gmail.com>
2023-06-16 11:53:47 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'To print alphabet lowercase'
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(\n');
+
+	return (0);
+}
