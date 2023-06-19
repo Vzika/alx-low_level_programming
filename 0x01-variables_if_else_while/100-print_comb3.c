d00e0504e51613523d695750268174864542265c
Vzika <ngozi@gmail.com>
2023-06-19 15:02:29 -0700

    task100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,16 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = '0'; num1 < '9'; num1++)
+	for (num1 = 0; num1 < 100; num1++)
 	{
-		for (num2 = num1 + 1; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 8 && num2 == 9)
-				continue;
+			if (num1 != num2)
 			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num1 == 99 && num2 == 99)
+					break;
 				putchar(',');
 				putchar(' ');
 			}
