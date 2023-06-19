5eec55c289291b93fe6fc08d757775e94a63b8d8
Vzika <ngozi@gmail.com>
2023-06-19 08:53:59 -0700

    task101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,33 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
-  *main - 'print all possible comb of two digit'
-  *Return: Always 0 (success)
+  * main - 'print all possible comb of two digit'
+  * Return: Always 0 (success)
   */
 int main(void)
 {
-	int num1;
-	int num2;
+	int num1 = '0';
+	int num2 = '0';
+	int num3 = '0';
 
-	for (num1 = 0; num1 < 9; num1++)
+	for (num3 = '0'; num3 <= '9'; num3++)
 	{
-		for (num2 = num1 + 1; num2 < 10; num2++)
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+		for (num2 = '0'; num2 <= 9; num2++)
+		{
+			for (num1 = '0'; num1 <= 9; num1++)
 			{
-				if (num1 == 8 && num2 == 9)
-					continue;
+				if (!((num1 == num2) || (num2 == num3) || (num2 > num1) || (num3 > num2)))
+				{
+					putchar(num3);
+					putchar(num2);
+					putchar(num1);
+					if (!(num1 == '9' && num3 == '7' && num2 == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-			putchar(',');
-			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
