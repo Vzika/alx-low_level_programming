a173f5bf0e636427f5f3336da766fefb39e910b3
obasi Ngozi <ndukaubangozi@gmail.com>
2023-10-02 06:39:32 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+  * main - to print the name of a program
+  * @argc - counts the number of arg
+  * @ argv - array of arguments
+  * return: always (0)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
