cb891ef349545485d1b339dbb0e623c7efff5fee
obasi Ngozi <ndukaubangozi@gmail.com>
2023-10-02 06:47:52 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - to print the number of arg
+  * @argc - counts the arguments passed
+  * @argv - array os arguments
+  * return: always (0)
+  */
+
+int main(int argc, char * argv[])
+{
+	printf("%d\n", argc);
+}
