7ad1673c0a0de138d12e81cbd4755b4964aaf0da
obasi Ngozi <ndukaubangozi@gmail.com>
2023-10-10 06:04:10 -0700

    task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-/**
-  *main - to print file name
-  *
-  *
-  * Return:Always 0 (success)
-  */
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
